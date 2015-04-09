6276e8c5e98c76a5c29b811d52049019268e9cfa
Or Peled <orppeled@gmail.com>
2015-04-09 16:56:25 +0300

    Simon game II

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,103 +6,107 @@
  * @description
  * # MainCtrl
  * Controller of the simonApp,
- * Taking wikipedia info.
+ * Using wiki info.
  */
 
 
 angular.module('simonApp')
-  .controller('MainCtrl', function ($animate, $q, $timeout, ngAudio, $scope) {
+  .controller('MainCtrl', function ($timeout, ngAudio, $scope) {
+    
     var MAX_ARRAY_SIZE = 2;
     var currentMoveIndex = 0;
     var maximumStepsAchievedIndex = 1;
-    $scope.expectedComputerMovesStack = [];
     var speedOfTimeOut = 50;
-    
+    var expectedComputerMovesStack = [];
     
     // Generate random array with values 0-3
     function returnRandomizedArray() {
+            
         var arrayToRandomize = [];
+        
         for (var i=0; i < MAX_ARRAY_SIZE; i++) {
             arrayToRandomize.push(Math.floor((Math.random() * 4) + 0)); 
         }
         return arrayToRandomize;
-    
     }
     
-    
+    // Play next computer pattern
     function computerStepsPlayed(){
-            // The user finished this challange.
-            maximumStepsAchievedIndex++;
-            currentMoveIndex = 0;
-            var i = 0;
-        (function play() {
+            
+        // The user finished this challange
+        maximumStepsAchievedIndex++;
+        currentMoveIndex = 0;
+        
+        var i = 0;
+        (function playStepsPlusOne() {
             setTimeout(function() {
-                console.log($scope.expectedComputerMovesStack[i]);
-                $scope.simulateClickEvent($scope.expectedComputerMovesStack[i]);
+                $scope.simulateClickEvent(expectedComputerMovesStack[i]);
                 i++;
                 if (i < maximumStepsAchievedIndex) {
-                    play();
+                    playStepsPlusOne();
                 }
             }, speedOfTimeOut + 500);
         })();
     }
     
-    // Reset the game
+    // Resetting the game using start button
     $scope.clickReset = function () {
-        // Removing animation.
+        
+        // Removing animation
         angular.element('.btn-game').removeClass('hinge');
-        angular.element('.win').removeClass('animated fadeInUpBig show');
-        $scope.expectedComputerMovesStack = returnRandomizedArray();
+        angular.element('.win').removeClass('fadeInUpBig show');
+        
+        // Getting a new randomized array
+        expectedComputerMovesStack = returnRandomizedArray();
         currentMoveIndex = 0;
         maximumStepsAchievedIndex = 0;
-        computerStepsPlayed();
         
+        // Play first pattern
+        computerStepsPlayed();
     };
     
-    // If a user clicked color, omit sound, match, and push to queue.
+    // If a user clicked color, omit sound, match, and push to queue
     $scope.clickColor = function (button) {
-        // If the user got this one right, continue.
-            if ($scope.expectedComputerMovesStack[currentMoveIndex].toString() === button.target.attributes.data.value) {
-                console.log('User succeeded this step. Clicked on ' + button.target.attributes.data.value);
-                currentMoveIndex++;
+        
+        var buttonColorValue = button.target.attributes.data.value;
+        // If the user got this one right, continue
+        if (expectedComputerMovesStack[currentMoveIndex].toString() === buttonColorValue) {
+            currentMoveIndex++;
             
-                // Check if user finished the game.
-                if (maximumStepsAchievedIndex === MAX_ARRAY_SIZE) {
-                    //User Won
-                    console.log('User won!!');
-                    angular.element('.win').addClass('animated fadeInUpBig show');
-                    return;
-                }
-                // Check if user finished this challange.
-                if (currentMoveIndex === maximumStepsAchievedIndex) {
-                    computerStepsPlayed();
-                }
-
-            } else {
-                console.log('User failed! Resetting.');
-                angular.element('.btn-game').addClass('hinge');
-                
-            }   
+            // Check if user finished the game
+            if (maximumStepsAchievedIndex === MAX_ARRAY_SIZE) {
+                //User Won
+                angular.element('.win').addClass('fadeInUpBig show');
+                return;
+            }
+            // Check if user finished this challange
+            if (currentMoveIndex === maximumStepsAchievedIndex) {
+                computerStepsPlayed();
+            }
+        } else {
+            // User failed
+            angular.element('.btn-game').addClass('hinge');
+        }   
     };
     
-    // Function to simulate clicking to indicate player what to do.
+    // Function to simulate clicking to indicate player what to do
     $scope.simulateClickEvent = function (id) {
         $timeout(function() {
-            
-            // Play as if clicked.
+
+            // Play as if clicked
             $scope.audio = ngAudio.load('sounds/' + (id + 1) + '.mp3');
             $scope.audio.play();        
+
+            // Click animation
+            var btnAnimationName = 'bounce';
+            var btnAnimationEndedEvents = 
+                'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
             
-            // Adding click animation and removing classes when done.
-            var animationName = 'bounce';
-            var animationEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            angular.element('#' + id).addClass(animationName)
-                .one(animationEvents, function () {
-                angular.element('#' + id).removeClass(animationName);                                      
+            // Adding animation and removing it once finished
+            angular.element('#' + id).addClass(btnAnimationName)
+                .one(btnAnimationEndedEvents, function () {
+                angular.element('#' + id).removeClass(btnAnimationName);                                      
             });
-            console.log('Simulated a clicked on ' + id);
         }, 2000);
-        //angular.element('#' + id).removeClass('bounce');
     };
-    
     });
